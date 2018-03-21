39b9b6786f08508c6fba51513605129e7f0f1833
Fabian Kędzierski <grafska@gmail.com>
2018-03-21 11:46:01 +0100

    Fix 'require' path

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
-require 'suport/factory_bot'
+require 'support/factory_bot'
 
 ActiveRecord::Migration.maintain_test_schema!
 
