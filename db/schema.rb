498f4b19b47eac07e11e9baeac42b5059115f6a8
Fabian Kędzierski <grafska@gmail.com>
2018-03-21 11:48:47 +0100

    Add resources for 'Price'

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_03_21_100941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "prices", force: :cascade do |t|
+    t.string "instrument"
+    t.decimal "ask"
+    t.decimal "bid"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
