e4f063eeb4876ab631137cc352e7e4be534be2f6
Fabian Kędzierski <grafska@gmail.com>
2018-03-24 11:26:21 +0100

    Add Prices to database

--- forex_server/data_collector.rb
+++ forex_server/data_collector.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'sql/data_collector_sql'
+
 module ForexServer
   class DataCollector
     def save
       puts '-- Save instruments data'
+      ForexServer::SqlManager.instance.call(
+        ForexServer::DataCollectorSql.insert_price,
+        [0, 1.009, 1.0013, Time.now, Time.now, Time.now]
+      )
     end
   end
 end
