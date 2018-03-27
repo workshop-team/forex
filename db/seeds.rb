e00de26cc70eb81a6998a8571eea8ebe2c8407dd
Fabian Kędzierski <grafska@gmail.com>
2018-03-27 13:46:59 +0200

    Modify seeds adding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-Instrument.destroy_all
-Instrument.create(name: 'eur_usd', label: 'EUR/USD')
-Instrument.create(name: 'eur_jpy', label: 'EUR/JPY')
-Instrument.create(name: 'gbp_usd', label: 'GBP/USD')
-Instrument.create(name: 'aud_usd', label: 'AUD/USD')
-Instrument.create(name: 'nzd_usd', label: 'NZD/USD')
+Instrument.find_or_create_by(name: 'eur_usd', label: 'EUR/USD')
+Instrument.find_or_create_by(name: 'eur_jpy', label: 'EUR/JPY')
+Instrument.find_or_create_by(name: 'gbp_usd', label: 'GBP/USD')
+Instrument.find_or_create_by(name: 'aud_usd', label: 'AUD/USD')
+Instrument.find_or_create_by(name: 'nzd_usd', label: 'NZD/USD')
+
+puts 'Seeds were added.'
