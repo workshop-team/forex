ea6b378f3e2df0257221d79d827dc02708b53e66
Fabian Kędzierski <grafska@gmail.com>
2018-03-28 10:49:42 +0200

    Add Strategy associations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Instrument < ApplicationRecord
+  has_many :strategies
+
   validates :name, :label, presence: true
 end
--- app/models/strategy_logic.rb
+++ app/models/strategy_logic.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class StrategyLogic < ApplicationRecord
+  has_many :strategies
+
   validates :class_name, :label, presence: true
 end
