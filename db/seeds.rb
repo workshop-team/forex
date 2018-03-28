fd33390f68038e88f00eb28a3e9b6c54397b91d8
Fabian Kędzierski <grafska@gmail.com>
2018-03-28 09:57:39 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@ Instrument.find_or_create_by(name: 'gbp_usd', label: 'GBP/USD')
 Instrument.find_or_create_by(name: 'aud_usd', label: 'AUD/USD')
 Instrument.find_or_create_by(name: 'nzd_usd', label: 'NZD/USD')
 
+StrategyLogic.find_or_create_by(class_name: 'StrategyLogic1', label: 'Strategy Logic 1')
+
 puts 'Seeds were added.'
