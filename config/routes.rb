836b87f117ddd23619f15c1ee00f773befa60177
Nikodem Gorak <nikos83@gmail.com>
2018-03-30 22:23:15 +0200

    Fix haml scaffold in forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :strategies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'strategies#index'
+  get 'home' => 'pages#home'
 end
