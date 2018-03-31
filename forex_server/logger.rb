ecac9dc09feb77b158e92a95513e5206e3921e2c
Fabian Kędzierski <grafska@gmail.com>
2018-03-31 18:36:57 +0200

    Modify logger and sql_manager class

--- forex_server/logger.rb
+++ forex_server/logger.rb
@@ -7,11 +7,15 @@ module ForexServer
   class Logger
     include Singleton
 
-    def save(info, type = 'info')
+    URL = 'http://localhost:3000/requests/send_notification'
+
+    def call(info, type = 'info')
       ForexServer::SqlManager.instance.call(
         ForexServer::LoggerSql.insert_notification,
         [type, info, Time.now, Time.now]
       )
+
+      ForexServer::HttpRequester.instance.call('post', URL, info: info, type: type)
     end
   end
 end
