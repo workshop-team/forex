0a92b2d73fac7d4359dd5057719bfdbf082e2ce3
Nikodem Gorak <nikos83@gmail.com>
2018-03-31 16:43:35 +0200

    Add translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :strategies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'strategies#index'
+  resources :strategies
   get 'home' => 'pages#home'
 end
