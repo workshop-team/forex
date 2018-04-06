169bbf9bc084294ed3de9c4e29c2f26809701ae1
Fabian Kędzierski <grafska@gmail.com>
2018-04-06 12:25:24 +0200

    Refactor SQL sources

--- forex_server/logger.rb
+++ forex_server/logger.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'singleton'
-require_relative 'sql/logger_sql'
+require_relative 'sql/notifications_sql'
 
 module ForexServer
   class Logger
@@ -10,9 +10,11 @@ module ForexServer
     URL = 'http://localhost:3000/requests/send_notification'
 
     def call(info, type = 'info')
+      current_time = Time.now
+
       ForexServer::SqlManager.instance.call(
-        ForexServer::LoggerSql.insert_notification,
-        [type, info, Time.now, Time.now]
+        ForexServer::NotificationsSql.insert_notification,
+        [type, info, current_time, current_time]
       )
 
       ForexServer::HttpRequester.instance.call('post', URL, info: info, type: type)
