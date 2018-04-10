f273595f833754fb8fc34dc5d0fe726307c69b7a
Fabian Kędzierski <grafska@gmail.com>
2018-04-10 15:45:10 +0200

    Use Devise in test and fix controller tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
   render_views
+  let(:user) { create(:user) }
+
+  before { sign_in user }
 
   describe 'GET home' do
     it 'returns http success' do
