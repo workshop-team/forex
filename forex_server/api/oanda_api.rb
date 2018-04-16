36305d25396da08d63c162271b5a316f492bf3a0
Fabian Kędzierski <grafska@gmail.com>
2018-04-16 14:00:59 +0200

    Implement Oanda API class

---
+++ forex_server/api/oanda_api.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'singleton'
+require_relative 'http_requester'
+
+module ForexServer
+  class OandaApi < HttpRequester
+    include Singleton
+
+    URL = 'https://api-fxpractice.oanda.com/v3/'
+    HEADERS = {
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer #{ENV['OANDA_TOKEN']}"
+    }.freeze
+
+    def last_price(instrument, params = { count: 1, granularity: 'S5', smooth: true })
+      endpoint = "instruments/#{instrument}/candles"
+      prepare_call('get', endpoint, params)
+    end
+
+    private
+
+    def prepare_call(method, endpoint, params)
+      call(method, "#{URL}#{endpoint}", HEADERS, params)
+    end
+  end
+end
