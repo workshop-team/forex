1118f2f5b37d24b22918e0d07c287ca4d528445b
Fabian Kędzierski <grafska@gmail.com>
2018-04-20 19:37:14 +0200

    Modify SqlManager to allow using Treads

--- forex_server/sql_manager.rb
+++ forex_server/sql_manager.rb
@@ -7,16 +7,16 @@ module ForexServer
   class SqlManager
     include Singleton
 
-    def initialize
-      @con = PG.connect dbname: 'forex_development' # , user: 'user_sample', password: 'pswd_sample'
-    end
-
     def call(query, params = [])
-      @con.exec_params(query, params)
+      # TODO: Setting credintals in .env and build a logic for using develop/production db
+      con = PG.connect dbname: 'forex_development' # , user: 'user_sample', password: 'pswd_sample'
+      con.exec_params(query, params)
     rescue PG::Error => e
       ForexServer::Logger.instance.call(e.message, 'error')
       puts e.message
       []
+    ensure
+      con&.close
     end
   end
 end
