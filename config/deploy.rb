3cd8430e6baba116c7615687f2b67c3a10f2de1a
Fabian Kędzierski <grafska@gmail.com>
2018-04-23 12:37:43 +0200

    Add and set up Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.10.1'
+
+set :application, 'forex'
+set :repo_url, 'git@github.com:workshop-team/forex.git'
+set :deploy_to, '/var/www/forex'
+
+append(
+  :linked_files
+)
+
+# Default value for linked_dirs is []
+append(
+  :linked_dirs,
+  'log',
+  'tmp/pids',
+  'tmp/cache',
+  'tmp/sockets',
+  'public/uploads'
+)
+
+set :keep_releases, 5
+set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
+set :passenger_restart_with_touch, true
