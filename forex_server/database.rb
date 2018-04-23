7c8d0beeb0077f5574fe455678c4f0b0a873b476
Fabian Kędzierski <grafska@gmail.com>
2018-04-23 15:40:14 +0200

    Fix database setting

--- forex_server/database.rb
+++ forex_server/database.rb
@@ -7,7 +7,7 @@ module ForexServer
     class << self
       def connection
         if Socket.gethostname == 'workshop-team'
-          PG.connect dbname: 'forex_production', user: 'forex', password: ENV['FOREX_DATABASE_PASSWORD']
+          PG.connect dbname: 'forex', user: 'forex', password: ENV['FOREX_DATABASE_PASSWORD']
         else
           PG.connect dbname: 'forex_development'
         end
