0947bed149fc0162d0e8e82ef09887c4c17c633e
Nikodem Górak <nikos83@gmail.com>
2018-05-04 20:34:56 +0200

    remove test code

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -18,12 +18,3 @@ $(document).on('expanded.pushMenu', function(){
 $(document).ready(function() {
   $.AdminLTE.layout.activate();
 });
-
-$(document).on('page:load', function() {
-  var o;
-  o = $.AdminLTE.options;
-  if (o.sidebarPushMenu) {
-    $.AdminLTE.pushMenu.activate(o.sidebarToggleSelector);
-  }
-  $.AdminLTE.layout.activate();
-});
