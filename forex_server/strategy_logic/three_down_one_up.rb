89956db51c2ec3db73b58b0e1fb6941ec14d2946
Fabian Kędzierski <grafska@gmail.com>
2018-05-04 20:27:27 +0200

    Refactor strategy logic '3down1up'

--- forex_server/strategy_logic/three_down_one_up.rb
+++ forex_server/strategy_logic/three_down_one_up.rb
@@ -4,10 +4,10 @@ require_relative 'strategy_logic'
 
 module ForexServer
   class ThreeDownOneUp < StrategyLogic
+    DOWNS = 3
+
     def init_data
-      {
-        count_down: 0
-      }
+      { count_down: 0 }
     end
 
     def buy?
@@ -21,19 +21,34 @@ module ForexServer
       # hash: data
 
       data[:count_down] += 1 if Candel.red?(last_price)
+      log_count_down
 
       if Candel.green?(last_price)
         count_down = data[:count_down]
         data[:count_down] = 0
 
-        count_down >= 3
+        count_down >= DOWNS
       else
         false
       end
     end
 
     def sell?
-      Candel.red?(last_price)
+      is_candel_red = Candel.red?(last_price)
+
+      if is_candel_red
+        init_data
+        data[:count_down] += 1
+        log_count_down
+      end
+
+      is_candel_red
+    end
+
+    private
+
+    def log_count_down
+      Logger.instance.call("(#{strategy.name}) :: Count down: #{data[:count_down]}") # if data[:count_down].positive?
     end
   end
 end
