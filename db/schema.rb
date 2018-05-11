07961e92ab0054e6aa20229b4a7805292b341521
Fabian Kędzierski <grafska@gmail.com>
2018-05-11 12:20:07 +0200

    Add fields 'pl' and 'half_spread_cost' to Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_121051) do
+ActiveRecord::Schema.define(version: 2018_05_11_090722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2018_05_04_121051) do
     t.integer "oanda_buy_order_id", null: false
     t.integer "oanda_sell_order_id"
     t.integer "status", default: 0
+    t.decimal "pl"
+    t.decimal "half_spread_cost"
     t.index ["strategy_id"], name: "index_orders_on_strategy_id"
   end
 
