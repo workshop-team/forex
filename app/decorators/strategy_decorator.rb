c221e7a02e9a9df6a81f81f6469ee6820e8307cf
Fabian Kędzierski <grafska@gmail.com>
2018-05-14 17:47:21 +0200

    Allow stopping/restart Strategy

--- app/decorators/strategy_decorator.rb
+++ app/decorators/strategy_decorator.rb
@@ -6,4 +6,26 @@ class StrategyDecorator < Draper::Decorator
   def units
     object.units.nil? ? I18n.t('auto') : object.units
   end
+
+  def manager
+    h.content_tag(:a, manager_label, href: h.change_status_strategy_path(id: object.id, status: object.status))
+  end
+
+  def status
+    clazz = object.status == 'activated' ? 'text-success' : 'text-warning'
+    h.content_tag :span, object.status, class: clazz
+  end
+
+  private
+
+  def manager_label
+    case object.status
+    when 'created'
+      'Start'
+    when 'activated'
+      'Stop'
+    when 'stopped'
+      'Restart'
+    end
+  end
 end
